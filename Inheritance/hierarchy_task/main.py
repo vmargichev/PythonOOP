9317296ee1d053513346e3bcf272b4fb351bba21
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-03 14:34:14 +0200

    Completed Task

---
+++ Inheritance/hierarchy_task/main.py
@@ -0,0 +1,31 @@
+class Human:
+    def __init__(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+
+class Archer(Human):
+    def __init__(self, name, num_arrows):
+        super().__init__(name)
+        self.__num_arrows = num_arrows
+
+    def get_num_arrows(self):
+        return self.__num_arrows
+
+    def use_arrows(self, num):
+        if self.__num_arrows < num:
+            raise Exception("not enough arrows")
+        self.__num_arrows -= num
+
+
+class Crossbowman(Archer):
+    def __init__(self, name, num_arrows):
+        super().__init__(name, num_arrows)
+
+    def triple_shot(self, target):
+        if self.get_num_arrows() < 3:
+            raise Exception("not enough arrows")
+        self.use_arrows(3)
+        return f"{target.get_name()} was shot by 3 crossbow bolts"
