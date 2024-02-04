1fca154262ddfd1998929a433879e08b2fe4e05d
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-04 21:16:49 +0200

    Completed Task

---
+++ Polymorphism/overloading_task/main.py
@@ -0,0 +1,11 @@
+class Sword:
+    def __init__(self, sword_type):
+        self.sword_type = sword_type
+
+    def __add__(self, other):
+        if self.sword_type == "bronze" and other.sword_type == "bronze":
+            return Sword("iron")
+        elif self.sword_type == "iron" and other.sword_type == "iron":
+            return Sword("steel")
+        else:
+            raise Exception("can not craft")
