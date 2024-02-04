f9e5a23a822371b9e1a9482b25e0dcb95c06b9b3
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-04 20:19:48 +0200

    Final one I promise

--- Polymorphism/rectangle_task/main.py
+++ Polymorphism/rectangle_task/main.py
@@ -1,4 +1,38 @@
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x_1, y_1, x_2, y_2):
+        pass
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, height, width, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.fire_range = fire_range
+        self.height = height
+        self.width = width
+        self.__hit_box = Rectangle(pos_x - width / 2, pos_y - height / 2, pos_x + width / 2, pos_y + height / 2)
+
+    def in_area(self, x_1, y_1, x_2, y_2):
+        rect = Rectangle(x_1, y_1, x_2, y_2)
+        return (rect.overlaps(self.__hit_box))
+
+
+# Don't touch below this line
+
+
 class Rectangle:
+    def overlaps(self, rect):
+        return (
+            self.get_left_x() <= rect.get_right_x()
+            and self.get_right_x() >= rect.get_left_x()
+            and self.get_top_y() >= rect.get_bottom_y()
+            and self.get_bottom_y() <= rect.get_top_y()
+        )
+
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
         self.y1 = y1
@@ -6,42 +40,21 @@ class Rectangle:
         self.y2 = y2
 
     def get_left_x(self):
-        if self.x1 > self.x2:
-            return self.x2
-        else:
+        if self.x1 < self.x2:
             return self.x1
+        return self.x2
 
     def get_right_x(self):
         if self.x1 > self.x2:
             return self.x1
-        else:
-            return self.x2
+        return self.x2
 
     def get_top_y(self):
         if self.y1 > self.y2:
             return self.y1
-        else:
-            return self.y2
+        return self.y2
 
     def get_bottom_y(self):
-        if self.y1 > self.y2:
-            return self.y2
-        else:
+        if self.y1 < self.y2:
             return self.y1
-        
-    def overlaps(self, rect):
-        if self.get_left_x() == rect.get_right_x() or self.get_left_x() < rect.get_right_x():
-            if self.get_right_x() == rect.get_left_x() or self.get_right_x() > rect.get_left_x():
-                if self.get_top_y() == rect.get_bottom_y() or self.get_top_y() > rect.get_bottom_y():
-                    if self.get_bottom_y() == rect.get_top_y() or self.get_bottom_y() < rect.get_top_y():
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return False
-        
-
+        return self.y2
