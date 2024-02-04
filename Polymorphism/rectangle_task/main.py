f77536d74aabfec1e0832b523e5c1e986d91d13f
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-04 18:58:21 +0200

    Part two is done

--- Polymorphism/rectangle_task/main.py
+++ Polymorphism/rectangle_task/main.py
@@ -4,3 +4,27 @@ class Rectangle:
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+
+    def get_left_x(self):
+        if self.x1 > self.x2:
+            return self.x2
+        else:
+            return self.x1
+
+    def get_right_x(self):
+        if self.x1 > self.x2:
+            return self.x1
+        else:
+            return self.x2
+
+    def get_top_y(self):
+        if self.y1 > self.y2:
+            return self.y1
+        else:
+            return self.y2
+
+    def get_bottom_y(self):
+        if self.y1 > self.y2:
+            return self.y2
+        else:
+            return self.y1
